8a216de6f3bc6880cac32828f467442acc2eca34
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-12-22 19:32:14 +0700

    using tinymce

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Tutorial
+from tinymce import TinyMCE
+from django.db import models
+
 
 # Register your models here.
+class TutorialAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Title/Content', {'fields': ['tutorial_title', 'tutorial_content']}),
+        ('Date Published', {'fields': ['tutorial_publish']})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+    }
+
+
+admin.site.register(Tutorial, TutorialAdmin)
+
