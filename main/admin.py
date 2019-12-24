3862a4a37404176e12d39cbf02b7f1d415c071c6
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-12-24 19:25:35 +0700

    linking model with foreign key

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 from tinymce import TinyMCE
 from django.db import models
 
@@ -8,6 +8,8 @@ from django.db import models
 class TutorialAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Title/Content', {'fields': ['tutorial_title', 'tutorial_content']}),
+        ('URL', {'fields': ['tutorial_slug']}),
+        ('Series', {'fields': ['tutorial_series']}),
         ('Date Published', {'fields': ['tutorial_publish']})
     ]
 
@@ -16,5 +18,7 @@ class TutorialAdmin(admin.ModelAdmin):
     }
 
 
+admin.site.register(TutorialCategory)
+admin.site.register(TutorialSeries)
 admin.site.register(Tutorial, TutorialAdmin)
 
