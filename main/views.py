46ae1abb07ca8f89a996259a0a810434fbd46345
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-12-24 17:46:14 +0700

    includes and messages

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 def homepage(request):
@@ -11,16 +13,18 @@ def homepage(request):
 
 
 def register(request):
-
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'Account Created for {username}')
             login(request, user)
+            messages.info(request, f'You are now logged in as {username}')
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f'{msg}: {form.error_messages[msg]}')
 
     form = UserCreationForm
     context = {
