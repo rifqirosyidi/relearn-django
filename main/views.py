1c06ce81d90b04a9013e3f6eeec4c9575ad26bf6
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-12-24 17:12:15 +0700

    creating register

--- main/views.py
+++ main/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Tutorial
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def homepage(request):
     return render(request=request,
                   template_name='main/home.html',
                   context={'tutorials': Tutorial.objects.all()})
+
+
+def register(request):
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("main:homepage")
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+    form = UserCreationForm
+    context = {
+        'form': form
+    }
+    return render(request, 'main/register.html', context)
+
